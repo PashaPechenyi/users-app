7d49e1b6e67dcaf4e7ef07ed265a2443ae0f25c0
PashaPechenyi <pashapechenn@gmail.com>
2020-05-30 18:46:53 +0300

    Updates

---
+++ precache-manifest.e47416d0e59792161443fc62cea4bed8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d513621e8fa693aba696b18e0afbc11a",
+    "url": "/users-app/index.html"
+  },
+  {
+    "revision": "db87e955d61a3791803d",
+    "url": "/users-app/static/css/main.1c0c33f9.chunk.css"
+  },
+  {
+    "revision": "794d0d1b5c476723cdee",
+    "url": "/users-app/static/js/2.0bf26393.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/users-app/static/js/2.0bf26393.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db87e955d61a3791803d",
+    "url": "/users-app/static/js/main.11481b4a.chunk.js"
+  },
+  {
+    "revision": "5a115caa8dd6fa428da7",
+    "url": "/users-app/static/js/runtime-main.26a74f2a.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/users-app/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "bce28fb772838de34344b20fa26296b0",
+    "url": "/users-app/static/media/logo.bce28fb7.svg"
+  },
+  {
+    "revision": "f28cb247d5778a3e9e6e5be021b9a81b",
+    "url": "/users-app/static/media/man-laptop-v1.f28cb247.svg"
+  },
+  {
+    "revision": "63637e948daaff9637f3f5a803d2b669",
+    "url": "/users-app/static/media/menu icon.63637e94.svg"
+  }
+]);
